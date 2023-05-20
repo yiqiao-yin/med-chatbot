a2ea8ba70e5637892502a0186136eaef8442ce14
Yiqiao Yin <eagle0504@gmail.com>
2023-05-20 17:10:18 -0400

    app updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 import os
 import urllib.request
 from pathlib import Path
-from typing import Any, Dict, List, Union
+from typing import Dict, List, Union
 
 import cv2
 import matplotlib.pyplot as plt
@@ -12,23 +12,11 @@ import numpy as np
 import openai
 import pandas as pd
 import shinyswatch
-import skimage
-import skimage.filters
-import skimage.io
-import skimage.util
-from PIL import Image, ImageOps
+from htmltools import css
 from scipy.spatial.distance import cosine
 
 # from sentence_transformers import SentenceTransformer
 from shiny import *
-from shiny import App, render, ui
-from shiny.types import FileInfo, ImgData, SilentException
-# from textractcaller import Textract_Features, call_textract
-# from textractprettyprinter.t_pretty_print import (
-#     Pretty_Print_Table_Format,
-#     Textract_Pretty_Print,
-#     get_string,
-# )
 
 openai.api_key = "ENTER API KEY HERE"
 
@@ -226,28 +214,66 @@ def get_completion_from_messages(
     return response.choices[0].message["content"]
 
 
+# def split_paragraph(paragraph: str) -> str:
+#     """
+#     Takes a long paragraph as input, splits it every 50 characters and adds a newline character "\n"
+#     so that each line is no more than 50 characters long.
+
+#     :param paragraph: The long paragraph to be split.
+#     :type paragraph: str
+#     :return: A string with newline characters added after every 50 characters.
+#     :rtype: str
+#     """
+#     sentences = paragraph.split(". ")
+#     new_paragraph = ""
+#     for sentence in sentences:
+#         words = sentence.split()
+#         new_sentence = ""
+#         for word in words:
+#             if len(new_sentence + word) > 40:
+#                 new_paragraph += new_sentence.strip() + "\n"
+#                 new_sentence = ""
+#             new_sentence += word + " "
+#         new_paragraph += new_sentence.strip() + ". "
+#     return new_paragraph[:-2]  # Remove the last period and space
+
+
 def split_paragraph(paragraph: str) -> str:
     """
-    Takes a long paragraph as input, splits it every 50 characters and adds a newline character "\n"
-    so that each line is no more than 50 characters long.
+    Takes in a paragraph as a string and returns a new paragraph where each line has 100 words or fewer.
 
-    :param paragraph: The long paragraph to be split.
-    :type paragraph: str
-    :return: A string with newline characters added after every 50 characters.
-    :rtype: str
+    Args:
+    - paragraph: A string containing a large paragraph
+
+    Returns:
+    - A string containing the paragraph split into multiple lines
     """
-    sentences = paragraph.split(". ")
-    new_paragraph = ""
-    for sentence in sentences:
-        words = sentence.split()
-        new_sentence = ""
-        for word in words:
-            if len(new_sentence + word) > 40:
-                new_paragraph += new_sentence.strip() + "\n"
-                new_sentence = ""
-            new_sentence += word + " "
-        new_paragraph += new_sentence.strip() + ". "
-    return new_paragraph[:-2]  # Remove the last period and space
+
+    # Split the original paragraph into individual words
+    words = paragraph.split()
+
+    # Initialize an empty list to store lines
+    lines = []
+
+    # Initialize a variable to keep track of the current line length
+    current_line_length = 0
+
+    # Iterate through all the words
+    for word in words:
+        # If adding the current word would exceed the 100-token limit,
+        # add the current line to the 'lines' list and start a new line
+        if current_line_length + len(word) > 100:
+            lines.append("\n")
+            current_line_length = 0
+
+        # Add the current word to the current line, along with a space
+        lines.append(word + " ")
+
+        # Update the current line length
+        current_line_length += len(word) + 1
+
+    # Join all the lines together into a single string and return it
+    return "".join(lines)
 
 
 def calculate_cosine_similarity(sentence1: str, sentence2: str) -> float:
@@ -299,55 +325,7 @@ def add_dist_score_column(dataframe: pd.DataFrame, sentence: str) -> pd.DataFram
     )
     sorted_dataframe = dataframe.sort_values(by="sts_score", ascending=False)
 
-    return sorted_dataframe.iloc[:15, :]
-
-
-def get_image(file_infos: List[Dict[str, Any]]):
-    """
-    Reads an image file, resizes it to 1000 pixels wide,
-    converts it to grayscale, rotates it based on EXIF tag,
-    applies thresholding, saves the image, and returns nothing.
-
-    Args:
-        None.
-
-    Returns:
-        None.
-
-    Raises:
-        SilentException: If no file information is received.
-    """
-
-    # Get file information
-    # file_infos: List[Dict[str, Any]] = input.file()
-
-    if not file_infos:
-        raise SilentException()
-
-    # Open image
-    file_info: Dict[str, Union[str, int]] = file_infos[0]
-    img: Image.Image = Image.open(file_info["datapath"])
-
-    # Resize to 1000 pixels wide
-    basewidth: int = 600
-    wpercent: float = basewidth / float(img.size[0])
-    hsize: int = int((float(img.size[1]) * float(wpercent)))
-    img: Image.Image = img.resize((basewidth, hsize), Image.ANTIALIAS)
-
-    # Convert to grayscale
-    img: Image.Image = ImageOps.grayscale(img)
-
-    # Rotate image based on EXIF tag
-    img: Image.Image = ImageOps.exif_transpose(img)
-
-    # Convert to numpy array for skimage processing
-    image_data: np.ndarray = np.array(img)
-
-    # Apply thresholding
-    val: float = skimage.filters.threshold_otsu(image_data)
-    mask: np.ndarray = image_data < val
-
-    return mask
+    return sorted_dataframe.iloc[:5, :]
 
 
 app_ui = ui.page_fluid(
@@ -356,26 +334,90 @@ app_ui = ui.page_fluid(
     #  cerulean, cosmo, cyborg, darkly, flatly, journal, litera, lumen, lux,
     #  materia, minty, morph, pulse, quartz, sandstone, simplex, sketchy, slate,
     #  solar, spacelab, superhero, united, vapor, yeti, zephyr
-    shinyswatch.theme.cyborg(),
+    shinyswatch.theme.slate(),
     ui.navset_tab(
         # elements ----
         ui.nav(
-            "Clinical Trial ChatBot",
+            "Yin's ChatBot",
             ui.layout_sidebar(
                 ui.panel_sidebar(
                     ui.row(
                         ui.column(
                             8,
                             ui.div(
-                                ui.input_action_button(
-                                    "clinical_trials_chatbot_btn",
-                                    "Ask me anything!",
-                                )
+                                ui.input_text(
+                                    "conditional_input_ai_name",
+                                    "Name your AI assistant 请输入AI助手的名字",
+                                    placeholder="Ava",
+                                ),
                             ),
                         )
-                    )
+                    ),
+                    ui.row(
+                        ui.column(
+                            8,
+                            ui.div(
+                                ui.input_select(
+                                    "conditional_input",
+                                    "Select input 选择下面一个选项",
+                                    # clinical_trials_qa.csv
+                                    # lh_ar2023.csv
+                                    {
+                                        "clinical_trials_qa": "Clinical Trials 临床试验",
+                                        "lh_ar2022": "LabCorp Annual Report 2022 徕博科2022年年报",
+                                    },
+                                ),
+                            ),
+                        )
+                    ),
+                    ui.row(
+                        ui.column(
+                            8,
+                            ui.div(
+                                ui.help_text(
+                                    "Please reach out to me if you want to add options. 想更新更多，请联系我. LinkedIn 领英链接: "
+                                ),
+                                ui.HTML(
+                                    "<a href='https://www.linkedin.com/in/yiqiaoyin/'>link</a>"
+                                ),
+                            ),
+                        )
+                    ),
                 ),
                 ui.panel_main(
+                    # ui.row(
+                    #     ui.column(
+                    #         12,
+                    #         ui.div(
+                    #             {"class": "app-col"},
+                    #             ui.output_text_verbatim(
+                    #                 "clinical_trials_chatbot_input_output"
+                    #             ),
+                    #             style=css(
+                    #                 display="flex",
+                    #                 border="5px solid grey",
+                    #                 justify_content="left",
+                    #                 align_items="left",
+                    #                 gap="2rem",
+                    #             ),
+                    #         ),
+                    #     )
+                    # ),
+                    ui.row(
+                        ui.column(
+                            8,
+                            ui.div(ui.h4("Conversation starts here 对话从这里开始:")),
+                        )
+                    ),
+                    ui.row(
+                        ui.column(
+                            12,
+                            ui.div(
+                                {"class": "app-col"},
+                                ui.output_ui("conversation_history"),
+                            ),
+                        )
+                    ),
                     ui.row(
                         ui.column(
                             12,
@@ -383,7 +425,7 @@ app_ui = ui.page_fluid(
                                 {"class": "app-col"},
                                 ui.input_text_area(
                                     "clinical_trials_chatbot_input",
-                                    "Enter your question here regarding clinical trials:",
+                                    "Enter your question here / 请输入您的问题:",
                                     width="100%",
                                     placeholder="What is the purpose of a clinical trial?",
                                 ),
@@ -392,92 +434,33 @@ app_ui = ui.page_fluid(
                     ),
                     ui.row(
                         ui.column(
-                            12,
+                            8,
                             ui.div(
-                                {"class": "app-col"},
-                                ui.output_text_verbatim(
-                                    "clinical_trials_chatbot_input_output"
-                                ),
+                                ui.input_action_button(
+                                    "clinical_trials_chatbot_btn",
+                                    "Click here to ask questions! 点击这里提问!",
+                                )
                             ),
                         )
                     ),
                 ),
             ),
         ),
-        # ui.nav(
-        #     "Snap and Ask",
-        #     ui.layout_sidebar(
-        #         ui.panel_sidebar(
-        #             ui.row(
-        #                 ui.column(
-        #                     8,
-        #                     ui.div(
-        #                         ui.input_file(
-        #                             "file",
-        #                             None,
-        #                             button_label="Open camera",
-        #                             # This tells it to accept still photos only (not videos).
-        #                             accept="image/*",
-        #                             # This tells it to use the phone's rear camera. Use "user" for the front camera.
-        #                             capture="environment",
-        #                         ),
-        #                     ),
-        #                 )
-        #             )
-        #         ),
-        #         ui.panel_main(
-        #             ui.row(
-        #                 ui.column(
-        #                     12,
-        #                     ui.output_image("image"),
-        #                 ),
-        #             ),
-        #             ui.row(
-        #                 ui.column(
-        #                     12,
-        #                     ui.output_text_verbatim("image_text"),
-        #                 ),
-        #             ),
-        #             ui.row(),
-        #             ui.row(
-        #                 ui.column(
-        #                     12,
-        #                     ui.div(
-        #                         {"class": "app-col"},
-        #                         # ui.input_text_area(
-        #                         #     "clinical_trials_chatbot_input",
-        #                         #     "Enter your question here regarding clinical trials:",
-        #                         #     width="100%",
-        #                         #     placeholder="What is the purpose of a clinical trial?",
-        #                         # ),
-        #                     ),
-        #                 )
-        #             ),
-        #             ui.row(
-        #                 ui.column(
-        #                     12,
-        #                     ui.div(
-        #                         {"class": "app-col"},
-        #                         # ui.output_text_verbatim(
-        #                         #     "clinical_trials_chatbot_input_output"
-        #                         # ),
-        #                     ),
-        #                 )
-        #             ),
-        #         ),
-        #     ),
-        # ),
     ),
 )
 
 
 def server(input, output, session):
-    x = reactive.Value("Conversation starts here: \n \n")
+    x = reactive.Value("")
 
     @reactive.Effect
     @reactive.event(input.clinical_trials_chatbot_btn)
     def _():
-        infile = Path(__file__).parent / "clinical_trials_qa.csv"
+        fname = input.conditional_input()
+        # available file names are:
+        # clinical_trials_qa.csv
+        # lh_ar2022.csv
+        infile = Path(__file__).parent / f"{fname}.csv"
         df = pd.read_csv(infile)
         # Use the DataFrame's to_html() function to convert it to an HTML table, and
         # then wrap with ui.HTML() so Shiny knows to treat it as raw HTML.
@@ -486,45 +469,65 @@ def server(input, output, session):
         qa_pairs = convert_to_list_of_dict(df_screened_by_dist_score)
         qa_pairs.append({"role": "user", "content": user_question})
         response = get_completion_from_messages(qa_pairs, temperature=1)
+        name_of_ai = input.conditional_input_ai_name()
         x.set(
             x()
             + " \n\nUser: "
             + user_question
-            + " \nAI Doctor: "
+            + " \naaaaa"
+            + name_of_ai
+            + ": "
             + split_paragraph(response)
             + "\n\n"
         )
 
-    @output
-    @render.text
-    @reactive.event(input.clinical_trials_chatbot_btn)
-    def clinical_trials_chatbot_input_output():
-        return str(x())
-
-    # @output
-    # @render.image
-    # async def image() -> ImgData:
-    #     mask = get_image(file_infos=input.file())
-
-    #     # Save for render.image
-    #     skimage.io.imsave("small.png", skimage.util.img_as_ubyte(mask))
-
-    #     return {"src": "small.png", "width": "100%"}
-
     # @output
     # @render.text
-    # def image_text():
-    #     mask = get_image(file_infos=input.file())
+    # @reactive.event(input.clinical_trials_chatbot_btn)
+    # def clinical_trials_chatbot_input_output():
+    #     return str(x())
 
-    #     # Save for render.image
-    #     skimage.io.imsave("small.png", skimage.util.img_as_ubyte(mask))
+    style_user = css(
+        background="linear-gradient(to bottom, #1b175d, #15275a)",
+        border_radius="10px",
+        border="2px solid grey",
+        justify_content="right",
+        align_items="right",
+        gap="4rem",
+    )
 
-    #     documentName = "small.png"
+    style_assist = css(
+        background="linear-gradient(to bottom, #142858, #2f3862)",
+        border_radius="10px",
+        border="2px solid grey",
+        justify_content="right",
+        align_items="right",
+        gap="4rem",
+    )
 
-    #     textract_json = call_textract(input_document=documentName)
-    #     final_output = get_string(textract_json=textract_json)
+    @output
+    @render.ui
+    @reactive.event(input.clinical_trials_chatbot_btn)
+    def conversation_history():
+        conv_output = ui.TagList(ui.div(ui.HTML("")))
+        for s in str(x()).split("\n\n"):
+            for s_sub in s.split("aaaaa"):
+                if "User" in s_sub:
+                    conv_output.append(
+                        ui.div(
+                            ui.HTML(s_sub),
+                            style=style_user,
+                        )
+                    )
+                else:
+                    conv_output.append(
+                        ui.div(
+                            ui.HTML(s_sub),
+                            style=style_assist,
+                        )
+                    )
 
-    #     return final_output
+        return conv_output
 
 
 app = App(app_ui, server, debug=True)
